daa4a568b0172d2527a69e14f993af035a9be3cd
wu <2667497807@qq.com>
2024-07-16 16:24:48 +0800

    update sadly

---
+++ build/bt_cpp/CMakeFiles/first_tree.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_tree.dir/src/first_tree.cpp.o"
+  "CMakeFiles/first_tree.dir/src/first_tree.cpp.o.d"
+  "first_tree"
+  "first_tree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first_tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/bt_cpp/CMakeFiles/tree_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tree_demo.dir/src/tree_demo.cpp.o"
+  "CMakeFiles/tree_demo.dir/src/tree_demo.cpp.o.d"
+  "tree_demo"
+  "tree_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tree_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
